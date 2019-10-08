53e632275a0eca5fdba06adc7d89d965114241d3
Eero Holmala <eero.holmala@Student.tut.fi>
2019-10-08 12:51:51 +0300

    Added handlebars and dotenv

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+var env = require('dotenv').config();
 var express = require('express');
+var path = require('path');
+var hbs = require('express-handlebars');
 var app = express();
 
+app.use('/static', express.static(path.join(__dirname, '/public')))
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname+'/views/layouts'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res)=>{
-    res.send("<h1>Hello!</h1>");
+    res.render('index', {
+        title: "Hello!"
+    });
 });
 
 app.listen(3000, ()=>{
